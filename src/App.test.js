b9f63464a49bf8d7218ca708396a66f3441b2dc9
Eero Nikkinen <eenokki@gmail.com>
2022-05-04 22:27:14 +0300

    delete todo test

--- src/App.test.js
+++ src/App.test.js
@@ -12,19 +12,24 @@ it('renders without crashing', () => {
 
 
 
-test('add todo',() => { 
+test('delete todo',() => { 
    render(<App />);
 
    const desc = screen.getByPlaceholderText('Description');
    fireEvent.change(desc, { target: { value: 'Go to coffee' } });
    const date = screen.getByPlaceholderText('Date');
    fireEvent.change(date, { target: { value: '29.01.2021' } })
-
+  
    const button = screen.getByText('Add');
    fireEvent.click(button);
 
    const tablecell = screen.getByText(/go to coffee/i);
    expect(tablecell).toBeInTheDocument();
+
+   const delbutton = screen.getByText('Clear');
+   fireEvent.click(delbutton);
+
+   expect(tablecell).not.toBeInTheDocument();
 
   })
 
